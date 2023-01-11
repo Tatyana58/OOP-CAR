179f480ea9b879bfdd04d3af27bd2391232a19c6
Tatyana58 <tatbir@rambler.ru>
2023-01-11 20:28:48 +0300

    Начало Создала класс Car и объекты

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,34 @@
+/*Создайте класс «Автомобиль», у которого есть:
+
+марка (brand),
+модель (model),
+объем двигателя в литрах (engineVolume),
+цвет кузова (color),
+год производства (year),
+страна сборки (country).
+Создайте объекты для следующих автомобилей:
+
+Lada Granta, 2015 год выпуска, сборка в России, желтого цвета, объем двигателя — 1,7 л.
+Audi A8 50 L TDI quattro, 2020 год выпуска, сборка в Германии, черный цвет кузова, объем двигателя — 3,0 л.
+BMW Z8, сборка в Германии в 2021 году, черный цвет кузова, объем — 3,0 л.
+Kia Sportage 4-го поколения, 2018 год выпуска, сборка в Южной Корее, цвет кузова — красный, объем двигателя — 2,4 л.
+Hyundai Avante, сборка в Южной Корее, цвет кузова — оранжевый, объем двигателя — 1,6 л, год выпуска — 2016 год.
+ */
 public class Car {
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
 }
