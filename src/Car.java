ac786ea60941d4fa021ece25a949e3a4b2ba2680
Tatyana58 <tatbir@rambler.ru>
2023-01-11 21:32:25 +0300

    Начало Создала класс Car и объекты Задание 2 Задание 1

--- src/Car.java
+++ src/Car.java
@@ -1,63 +1,23 @@
-import java.util.Objects;
-
 /*Создайте класс «Автомобиль», у которого есть:
-
 марка (brand),
 модель (model),
 объем двигателя в литрах (engineVolume),
 цвет кузова (color),
 год производства (year),
 страна сборки (country).
-Создайте объекты для следующих автомобилей:
-
-Lada Granta, 2015 год выпуска, сборка в России, желтого цвета, объем двигателя — 1,7 л.
-Audi A8 50 L TDI quattro, 2020 год выпуска, сборка в Германии, черный цвет кузова, объем двигателя — 3,0 л.
-BMW Z8, сборка в Германии в 2021 году, черный цвет кузова, объем — 3,0 л.
-Kia Sportage 4-го поколения, 2018 год выпуска, сборка в Южной Корее, цвет кузова — красный, объем двигателя — 2,4 л.
-Hyundai Avante, сборка в Южной Корее, цвет кузова — оранжевый, объем двигателя — 1,6 л, год выпуска — 2016 год.
-
-Задание 2
-Перепишите класс «Автомобиль» с использованием конструктора. Передайте все свойства созданных вами объектов машин с помощью конструктора.
-
-Выведите в консоль сообщение о каждом автомобиле с перечислением всех их характеристик.
-
-Критерии оценки
-В поле объекта использован конструктор.
-Свойства объектов передаются с помощью конструктора.
-В консоль выведено сообщение о каждом автомобиле с перечислением всех их характеристик.
+Подсказка
+Для вывода объекта в консоль можно определить у него метод toString().
  */
 public class Car {
-    private String brand;
-    private String model;
-    private double engineVolume;
-    private String color;
-    private int year;
-    private String country;
-
-    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Car car = (Car) o;
-        return Double.compare(car.engineVolume, engineVolume) == 0 && year == car.year && brand.equals(car.brand) && model.equals(car.model) && color.equals(car.color) && country.equals(car.country);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(brand, model, engineVolume, color, year, country);
-    }
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int productionYear;
+    String productionСountry;
 
     @Override
     public String toString(){
-        return "Автомобиль: Марка " + brand + ", модель " + model + ", "+ engineVolume + "л., цвет " + color + ", год выпуска " + year + ", страна производитель " + country;
+        return "Автомобиль: Марка " + brand + ", модель " + model + ", "+ engineVolume + "л., цвет " + color + ", год выпуска " + productionYear + ", страна производитель " + productionСountry;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,89 @@
 /*
-Задание 2
-Перепишите класс «Автомобиль» с использованием конструктора. Передайте все свойства созданных вами объектов машин с помощью конструктора.
-Выведите в консоль сообщение о каждом автомобиле с перечислением всех их характеристик.
+Задание 1
+Создайте класс «Автомобиль», у которого есть:
+марка (brand),
+модель (model),
+объем двигателя в литрах (engineVolume),
+цвет кузова (color),
+год производства (year),
+страна сборки (country).
+Создайте объекты для следующих автомобилей:
+Lada Granta, 2015 год выпуска, сборка в России, желтого цвета, объем двигателя — 1,7 л.
+Audi A8 50 L TDI quattro, 2020 год выпуска, сборка в Германии, черный цвет кузова, объем двигателя — 3,0 л.
+BMW Z8, сборка в Германии в 2021 году, черный цвет кузова, объем — 3,0 л.
+Kia Sportage 4-го поколения, 2018 год выпуска, сборка в Южной Корее, цвет кузова — красный, объем двигателя — 2,4 л.
+Hyundai Avante, сборка в Южной Корее, цвет кузова — оранжевый, объем двигателя — 1,6 л, год выпуска — 2016 год.
+Напишите программу, которая будет выводить в консоль информацию о каждом автомобиле и все указанные выше характеристики.
+Подсказка
+Для вывода объекта в консоль можно определить у него метод toString().
 Критерии оценки
-В поле объекта использован конструктор.
-Свойства объектов передаются с помощью конструктора.
-В консоль выведено сообщение о каждом автомобиле с перечислением всех их характеристик.
-
+Создан класс car со свойствами brand, model, engineVolume, color, productionYear, productionCountry.
+Создан объект Lada Granta со свойствами класса car.
+Создан объекта Audi A8 со свойствами класса car.
+Создан объект BMW Z8 со свойствами класса car.
+Создан объект Kia Sportage 4-го поколения со свойствами класса car.
+Создан объект Hyundai Avante со свойствами класса car.
+В консоль выводится информация о каждом автомобиле и все указанные характеристики.
+Переменные инициализированы согласно правилам camelCase.
  */
-
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Курс 2. Задание 2.");
-        Car[] car = new Car[5];
-        car[0] = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
-        car[1] = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
-        car[2] = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
-        car[3] = new Car("Kia", "Sportage 4", 2.4, "красный", 2018, "Южная Корея");
-        car[4] = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        System.out.println("Курс 2. Задание 1.");
 
-        for (int i = 0; i < car.length ; i++) {
-            System.out.println(car[i]);
-        }
+        //Lada Granta, 2015 год выпуска, сборка в России, желтого цвета, объем двигателя — 1,7 л.
+        Car ladaGranta = new Car();
+        ladaGranta.brand = "Lada";
+        ladaGranta.model = "Granta";
+        ladaGranta.engineVolume = 1.7;
+        ladaGranta.color = "Желтый";
+        ladaGranta.productionYear = 2015;
+        ladaGranta.productionСountry = "Россия";
+
+        System.out.println(ladaGranta);
+
+        //Audi A8 50 L TDI quattro, 2020 год выпуска, сборка в Германии, черный цвет кузова, объем двигателя — 3,0 л.
+        Car Audi = new Car();
+        Audi.brand = "Audi";
+        Audi.model = "A8 50 L TDI quattro";
+        Audi.engineVolume = 3.0;
+        Audi.color = "Черный";
+        Audi.productionYear = 2020;
+        Audi.productionСountry = "Германия";
+
+        System.out.println(Audi);
+
+        //BMW Z8, сборка в Германии в 2021 году, черный цвет кузова, объем — 3,0 л.
+        Car BMW = new Car();
+        BMW.brand = "BMW";
+        BMW.model = "Z8";
+        BMW.engineVolume = 3.0;
+        BMW.color = "Черный";
+        BMW.productionYear = 2021;
+        BMW.productionСountry = "Германия";
+
+        System.out.println(BMW);
+
+        //Kia Sportage 4-го поколения, 2018 год выпуска, сборка в Южной Корее, цвет кузова — красный, объем двигателя — 2,4 л.
+        Car Kia = new Car();
+        Kia.brand = "Kia";
+        Kia.model = "Sportage 4";
+        Kia.engineVolume = 2.4;
+        Kia.color = "Красный";
+        Kia.productionYear = 2018;
+        Kia.productionСountry = "Южная Корея";
+
+        System.out.println(Kia);
+
+        //Hyundai Avante, сборка в Южной Корее, цвет кузова — оранжевый, объем двигателя — 1,6 л, год выпуска — 2016 год.
+        Car Hyundai = new Car();
+        Hyundai.brand = "Hyundai";
+        Hyundai.model = "Avante";
+        Hyundai.engineVolume = 1.6;
+        Hyundai.color = "Оранжевый";
+        Hyundai.productionYear = 2016;
+        Hyundai.productionСountry = "Южная Корея";
+
+        System.out.println(Hyundai);
     }
 }
