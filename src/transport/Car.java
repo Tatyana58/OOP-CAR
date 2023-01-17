6c200d22cba3334f38fd8639c6223d93010f2455
Tatyana58 <tatbir@rambler.ru>
2023-01-17 23:35:50 +0300

    Начало Создала класс Car и объекты Задание 2 Задание 1 Задание 3 Добавила проверки. Перенесла класс CAR в пакет transport Добавили новый свойства в класс CAR Добавили Геттеры и Сеттеры Проверка резины по месяцу Вывела Печать данных в отдельный метод Задание 2 Создали class Key

--- src/transport/Car.java
+++ src/transport/Car.java
@@ -1,5 +1,6 @@
 package transport;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 /*Задание 2
 В классе Car создайте вложенный класс «Ключ» (Key) с параметрами:
@@ -16,7 +17,6 @@ import java.util.Objects;
 Создан конструктор для создания нового объекта класса.
  */
 public class Car {
-
     private final String brand;
     private final String model;
     private double engineVolume;
@@ -29,22 +29,67 @@ public class Car {
     private String registrationNumber;
     private final int numberSeats;
     private boolean rubber;
+    private KeyCar keyCar;
 
-    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry, String transmissionCar, String carBodyType, String registrationNumber, int numberSeats, Boolean rubber) {
+    public Car(String brand, String model,
+               double engineVolume,
+               String color,
+               int productionYear,
+               String productionCountry,
+               String transmissionCar,
+               String carBodyType,
+               String registrationNumber,
+               int numberSeats,
+               Boolean rubber,
+               KeyCar keyCar) {
         this.brand = (brand != null && brand.isEmpty() != true && brand.isBlank() != true) ? brand : "Default";
         this.model = (brand != null && model.isEmpty() != true && model.isBlank() != true) ? model : "Default";
         this.engineVolume = (engineVolume <= 0) ? engineVolume : 1.5;
         this.color = (color != null && color.isEmpty() != true && color.isBlank() != true) ? model : "Белый";
         this.productionYear = (productionYear <= 0) ? productionYear : 2000;
         this.productionCountry = (productionCountry != null && productionCountry.isEmpty() != true && productionCountry.isBlank() != true) ? productionCountry : "Default";
-        this.transmissionCar = (transmissionCar != null && transmissionCar.isEmpty() != true && transmissionCar.isBlank() != true) ? transmissionCar : "Default";
-        this.carBodyType = (carBodyType != null && carBodyType.isEmpty() != true && carBodyType.isBlank() != true) ? carBodyType : "Default";
-        this.registrationNumber = (registrationNumber != null && registrationNumber.isEmpty() != true && registrationNumber.isBlank() != true) ? registrationNumber : "Default";
+        this.transmissionCar = (transmissionCar != null && transmissionCar.isEmpty() != true && transmissionCar.isBlank() != true) ? transmissionCar : "Автомат";
+        this.carBodyType = (carBodyType != null && carBodyType.isEmpty() != true && carBodyType.isBlank() != true) ? carBodyType : "Седан";
+        this.registrationNumber = (registrationNumber != null && registrationNumber.isEmpty() != true && registrationNumber.isBlank() != true) ? checkingRegistrationNumber(registrationNumber) : "Default";
         this.numberSeats = (numberSeats > 0 && numberSeats <= 5) ? numberSeats : 2;
-        this.rubber = (rubber==true)? rubber:false;
+        this.rubber = (rubber != null) ? rubber : true;
+        this.keyCar = (keyCar != null) ? keyCar : null;
     }
 
-        public String getBrand () {
+    public static String checkingRegistrationNumber(String registrationNumber) {
+        if (!Pattern.matches("[а-я][0-9]{3}[а-я]{2}[0-9]{2}[0-9]{2,3}", registrationNumber)) {
+            return registrationNumber;
+        } else {
+            System.out.println("Номер введен не корректно");
+            return " Не верный номер ";
+        }
+    }
+
+    public static class KeyCar {
+        private final boolean remoteEngineStart;
+        private final boolean accessKeyless;
+
+        public KeyCar(boolean remoteEngineStart, boolean accessKeyless) {
+            this.remoteEngineStart = remoteEngineStart;
+            this.accessKeyless = accessKeyless;
+        }
+
+        @Override
+        public String toString() {
+            return ", удаленный запуск двигателя " + remoteEngineStart +
+                    ", бесключевой доступ " + accessKeyless;
+        }
+    }
+
+    public KeyCar getKeyCar() {
+        return keyCar;
+    }
+
+    public void setKeyCar(KeyCar keyCar) {
+        this.keyCar = keyCar;
+    }
+
+    public String getBrand () {
             return brand;
         }
 
@@ -108,21 +153,22 @@ public class Car {
             this.rubber = rubber;
         }
 
-        @Override
-        public boolean equals (Object o){
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Car car = (Car) o;
-            return Double.compare(car.engineVolume, engineVolume) == 0 && productionYear == car.productionYear && numberSeats == car.numberSeats && rubber == car.rubber && brand.equals(car.brand) && model.equals(car.model) && color.equals(car.color) && productionCountry.equals(car.productionCountry) && transmissionCar.equals(car.transmissionCar) && carBodyType.equals(car.carBodyType) && registrationNumber.equals(car.registrationNumber);
-        }
-
-        @Override
-        public int hashCode () {
-            return Objects.hash(brand, model, engineVolume, color, productionYear, productionCountry, transmissionCar, carBodyType, registrationNumber, numberSeats, rubber);
-        }
-
-        @Override
-        public String toString () {
-            return "Автомобиль: Марка " + brand + ", модель " + model + ", " + engineVolume + "л., цвет " + color + ", год выпуска " + productionYear + ", страна производитель " + productionCountry + ", коробка передач " + transmissionCar + ", тип кузова " + carBodyType + ", геристрационный номер " + registrationNumber + ", количестко мест " + numberSeats + ", резина " + rubber;
-        }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Double.compare(car.engineVolume, engineVolume) == 0 && productionYear == car.productionYear && numberSeats == car.numberSeats && rubber == car.rubber && brand.equals(car.brand) && model.equals(car.model) && color.equals(car.color) && productionCountry.equals(car.productionCountry) && transmissionCar.equals(car.transmissionCar) && carBodyType.equals(car.carBodyType) && registrationNumber.equals(car.registrationNumber) && keyCar.equals(car.keyCar);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, engineVolume, color, productionYear, productionCountry, transmissionCar, carBodyType, registrationNumber, numberSeats, rubber, keyCar);
+    }
+
+    @Override
+        public String toString () {
+            return "Автомобиль: Марка " + brand + ", модель " + model + ", " + engineVolume + "л., цвет " + color + ", год выпуска " + productionYear + ", страна производитель " + productionCountry + ", коробка передач " + transmissionCar + ", тип кузова " + carBodyType + ", геристрационный номер " + registrationNumber + ", количестко мест " + numberSeats + ", резина " + rubber+keyCar;
+        }
+
+}
