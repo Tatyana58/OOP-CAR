0c8dfe94fecc43d3e57b5bc9bb3eea9893674112
Tatyana58 <tatbir@rambler.ru>
2023-02-10 22:07:30 +0300

    Создала класс Механик

---
+++ src/transport/Mechanic.java
@@ -0,0 +1,20 @@
+package transport;
+
+public class Mechanic {
+    String nameMechanic;
+    String companyWork;
+
+    public Mechanic(String nameMechanic, String companyWork) {
+        this.nameMechanic = nameMechanic;
+        this.companyWork = companyWork;
+    }
+
+    public void performMaintenance() {//провести техобслуживание
+    }
+
+    public void fixCar() { // починить машину
+    }
+
+}
+
+
